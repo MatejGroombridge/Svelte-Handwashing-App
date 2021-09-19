5cd8a5415b51d4d1d3eeae77620516254dd03ec6
MatejGroombridge <matejdpg@gmail.com>
2021-09-19 11:23:55 +1000

    html done

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,26 @@
-<h1>Test</h1>
+<script>
+  import Timer from "./Timer.svelte";
+  import HowTo from "./HowTo.svelte";
+</script>
+
+<h1>Handwashing App</h1>
+
+<Timer />
+<HowTo />
+
+<h3>
+  <a href="http://www.who.int/gpsc/clean_hands_protection/en/">
+    Picture Source
+  </a>
+</h3>
+
+<h3>
+  <a href="http://www.freesound.org"> Sound Source </a>
+</h3>
+
+<style>
+  h1,
+  h3 {
+    text-align: center;
+  }
+</style>
